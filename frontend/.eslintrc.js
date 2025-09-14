5259e14807dac671a38cfae1cf7f8d8f95c8b0fc
LouisY06 <147222351+LouisY06@users.noreply.github.com>
2025-09-14 01:15:16 -0400

    vercel

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/no-redeclare': 'warn'
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    'no-unused-vars': 'off'
   }
 };
